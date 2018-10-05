b8274cba78f10631e081fbac7050b9c3a30971e2
Leonard Richardson <leonardr@segfault.org>
2018-10-05 14:42:45 -0400

    Made existing usage work.

--- lane.py
+++ lane.py
@@ -560,14 +560,14 @@ class FeaturedFacets(FacetsWithEntryPoint):
     """
 
     def __init__(self, minimum_featured_quality, uses_customlists=False,
-                 **kwargs):
+                 entrypoint=None, **kwargs):
         """Set up an object that finds featured books in a given
         WorkList.
 
         :param kwargs: Other arguments may be supplied based on user
             input, but the default implementation is to ignore them.
         """
-        super(FeaturedFacets, self).__init__(**kwargs)
+        super(FeaturedFacets, self).__init__(entrypoint=entrypoint, **kwargs)
         self.minimum_featured_quality = minimum_featured_quality
         self.uses_customlists = uses_customlists
 
